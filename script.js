18a6b860650779dc06239a4838b1a91efe55e277
Francina Martín <francinakb@gmail.com>
2022-06-30 16:50:22 -0300

    desafio opcional

--- script.js
+++ script.js
@@ -5,42 +5,45 @@ let palabraEsp
 let test = ""   //indica que es un valor que no esta predeterminado pero que va a llevar una string
 do {
     palabraEsp = prompt("Ingrese un color en español").toLowerCase()
-} while (palabraEsp == test)
 
-switch (palabraEsp) {
-    case "rojo":
-        alert("red")
-        break
-    case "azul":
-        alert("blue")
-        break
-    case "verde":
-        alert("green")
-        break
-    case "negro":
-        alert("black")
-        break
-    case "blanco":
-        alert("white")
-        break
-    case "rosa":
-        alert("pink")
-        break
-    case "amarillo":
-        alert("yellow")
-        break
-    case "gris":
-        alert("grey")
-        break
-    case "naranja":
-        alert("orange")
-        break
-        case "marron":
-            alert("brown")
+    switch (palabraEsp) {
+        case "rojo":
+            alert("red")
             break
-    default:
-        alert("Color aun no traducido")
-        break
-}
+            
+        case "azul":
+            alert("blue")
+            break
+        case "verde":
+            alert("green")
+            break
+        case "negro":
+            alert("black")
+            break
+        case "blanco":
+            alert("white")
+            break
+        case "rosa":
+            alert("pink")
+            break
+        case "amarillo":
+            alert("yellow")
+            break
+        case "gris":
+            alert("grey")
+            break
+        case "naranja":
+            alert("orange")
+            break
+            case "marron":
+                alert("brown")
+                break
+        default:
+            alert("Color aun no traducido")
+            break
+    }
+} while (palabraEsp != test)
+
+
 
 
